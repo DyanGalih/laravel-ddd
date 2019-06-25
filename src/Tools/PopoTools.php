ddbb702e11a5ae9a09ea6dd4d37c70ea2b8b524a
Dyan Galih <dyan.galih@gmail.com>
2019-06-25 07:18:29 +0700

    Faster PopoTools

--- src/Tools/PopoTools.php
+++ src/Tools/PopoTools.php
@@ -8,115 +8,66 @@
 
 namespace WebAppId\DDD\Tools;
 
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Collection as CollectionSupport;
-
 /**
  * @author: Dyan Galih<dyan.galih@gmail.com> https://dyangalih.com
  * Class PopoTools
  */
 class PopoTools
 {
-    /**
-     * Local cache of a property getters per class - optimize reflection code if the same object appears several times
-     * @var array
-     */
-    private $classPropertyGetters = array();
     
     /**
      * @param mixed $object
      * @return string|false
-     * @throws \ReflectionException
      */
     public function serializeJson($object)
     {
-        return json_encode($this->serializeInternal($object));
+        return json_encode($this->serialize($object));
     }
     
     /**
      * @param $object
      * @return array
-     * @throws \ReflectionException
      */
     public function serialize($object)
     {
-        return $this->serializeInternal($object);
-    }
-    
-    /**
-     * @param $object
-     * @return array
-     * @throws \ReflectionException
-     */
-    private function serializeInternal($object)
-    {
-        if ($object instanceof Collection || $object instanceof CollectionSupport) {
-            $result = $object;
-        } elseif (is_array($object)) {
-            $result = $this->serializeArray($object);
-        } elseif (is_object($object)) {
-            $result = $this->serializeObject($object);
-        } else {
-            $result = $object;
-        }
-        return $result;
-    }
-    
-    /**
-     * @param $object
-     * @return \ReflectionClass
-     * @throws \ReflectionException
-     */
-    private function getClassPropertyGetters($object)
-    {
-        $className = get_class($object);
-        if (!isset($this->classPropertyGetters[$className])) {
-            $reflector = new \ReflectionClass($className);
-            $properties = $reflector->getProperties();
-            $getters = array();
-            foreach ($properties as $property) {
-                $name = $property->getName();
-                $getter = "get" . str_replace("_", "", ucfirst($name));
-                try {
-                    $reflector->getMethod($getter);
-                    $getters[$name] = $getter;
-                } catch (\Exception $e) {
-                    report($e);
-                    // if no getter for a specific property - ignore it
-                }
+        $objectAsArray = (array) $object;
+        foreach ($objectAsArray as $key => $value) {
+            if (stripos($key, "\0") === 0) {
+                $newKey = $this->fixKeyName($key);
+                $this->replaceKey($objectAsArray, $key, $newKey);
             }
-            
-            $this->classPropertyGetters[$className] = $getters;
+        
+            if (is_object($value)) {
+                $objectAsArray[$newKey] = $this->serialize($objectAsArray[$newKey]);
+            }
         }
-        return $this->classPropertyGetters[$className];
-    }
     
-    /**
-     * @param $object
-     * @return array
-     * @throws \ReflectionException
-     */
-    private function serializeObject($object)
-    {
-        $properties = $this->getClassPropertyGetters($object);
-        $data = array();
-        foreach ($properties as $name => $property) {
-            $data[$name] = $this->serializeInternal($object->$property());
-        }
-        return $data;
+        return $objectAsArray;
     }
     
     /**
      * @param $array
-     * @return array
-     * @throws \ReflectionException
+     * @param $curkey
+     * @param $newkey
+     * @return bool
      */
-    private function serializeArray($array)
+    function replaceKey(&$array, $curkey, $newkey)
     {
-        $result = array();
-        foreach ($array as $key => $value) {
-            $result[$key] = $this->serializeInternal($value);
+        if (array_key_exists($curkey, $array)) {
+            $array[$newkey] = $array[$curkey];
+            unset($array[$curkey]);
+            return true;
         }
-        return $result;
+        
+        return false;
+    }
+    
+    /**
+     * @param string $oldKey
+     * @return string
+     */
+    function fixKeyName(string $oldKey) : string
+    {
+        return substr($oldKey, strpos($oldKey, "\0", 2) + 1);
     }
 }
